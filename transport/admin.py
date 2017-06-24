9ca828e8200ebcb8e739cd649563cdc5d4cbf4c3
amar061996 <amar.prakash196@gmail.com>
2017-06-24 19:44:59 +0530

    Created database for Transport Location

--- transport/admin.py
+++ transport/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Transport
+from .models import Transport,TransportLocation
 
 admin.site.register(Transport)
+admin.site.register(TransportLocation)
